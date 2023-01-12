ba4a4b3d0f9fd0c24c66c710146c5efaafa7d404
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2023-01-12 23:10:28 +0700

    Update code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,17 +22,76 @@ import {
 import { useState } from "react";
 
 export const Navbar = () => {
-  const NavLink = styled(Typography)(({ theme }) => ({}));
+  const NavLink = styled(Typography)(({ theme }) => ({
+    fontSize: "14px",
+    color: "#4F5361",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      color: "#fff",
+    },
+  }));
 
-  const NavbarLinksBox = styled(Box)(({ theme }) => ({}));
+  const NavbarLinksBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
 
-  const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({}));
+  const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
+    cursor: "pointer",
+    display: "none",
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      display: "block",
+    },
+  }));
 
-  const NavbarContainer = styled(Container)(({ theme }) => ({}));
+  const NavbarContainer = styled(Container)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: theme.spacing(5),
+    [theme.breakpoints.down("md")]: {
+      padding: theme.spacing(2),
+    },
+  }));
 
-  const NavbarLogo = styled("img")(({ theme }) => ({}));
+  const NavbarLogo = styled("img")(({ theme }) => ({
+    cursor: "pointer",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
 
-  return <></>;
+  return (
+    <NavbarContainer>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <CustomMenuIcon />
+          <NavbarLogo src={logoImg} alt="logoImg" />
+        </Box>
+        <NavbarLinksBox>
+          <NavLink variant="body2">Home</NavLink>
+          <NavLink variant="body2">Features</NavLink>
+          <NavLink variant="body2">Services</NavLink>
+          <NavLink variant="body2">Listed</NavLink>
+          <NavLink variant="body2">Contact</NavLink>
+        </NavbarLinksBox>
+      </Box>
+    </NavbarContainer>
+  );
 };
 
 export default Navbar;
